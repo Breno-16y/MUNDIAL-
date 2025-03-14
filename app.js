226fa6f7154d9eb67a28ef43a22bfcae8d0b9427
Breno-16y <brenogomesde.l.andrade@gmail.com>
2025-03-14 17:53:53 -0300

    Update app.js
    Signed-off-by: Breno-16y <brenogomesde.l.andrade@gmail.com>

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ function handleLogin(event) {
     const password = document.getElementById("password").value;
 
     // Simulação de verificação de login (pode ser substituído por autenticação real)
-    if (username === "admin" && password === "123456") {
+    if (username === "brenomundial@gmail.com" && password === "09/04/2008") {
         alert("Login bem-sucedido!");
         // Redirecionar ou alterar para uma página de conteúdo (apenas exemplo de alert)
         window.location.href = "home.html"; // Você pode criar uma página home.html ou redirecionar conforme necessário
