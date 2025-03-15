03a91a66f61a9a057cd38211eca01bf9dff05fb5
Breno-16y <brenogomesde.l.andrade@gmail.com>
2025-03-14 21:51:14 -0300

    Update app.js
    Signed-off-by: Breno-16y <brenogomesde.l.andrade@gmail.com>

--- app.js
+++ app.js
@@ -1,30 +1,23 @@
-// Função de login
 function handleLogin(event) {
     event.preventDefault();
 
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
-    const successMessage = document.getElementById("successMessage");
+    const loginModal = document.getElementById("loginModal");
 
-    // Simulação de verificação de login (substitua com um backend real)
     if (username === "brenogomesmundial" && password === "09042008") {
-        // Exibe a mensagem de sucesso
-        successMessage.style.display = "block";
+        loginModal.style.display = "flex";
 
-        // Aguarda 2 segundos, depois oculta a mensagem e redireciona
         setTimeout(() => {
-            successMessage.style.opacity = "0"; // Suaviza a saída
-            setTimeout(() => {
-                window.location.href = "home.html"; // Redireciona após a mensagem sumir
-            }, 500);
+            loginModal.style.display = "none";
+            window.location.href = "home.html";
         }, 2000);
     } else {
         alert("Usuário ou senha incorretos.");
     }
 }
 
-// Função de logout
 function logout() {
     alert("Você foi desconectado.");
-    window.location.href = "index.html"; // Redireciona para a página de login
+    window.location.href = "index.html";
 }
